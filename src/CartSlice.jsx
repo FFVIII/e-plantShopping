ff4a0fb98e267dcc5339b26bcd12bdfe6e17c4de
FFVIII <fangvsyu@gmail.com>
2024-12-15 23:33:23 -0600

    fix bug

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,26 +4,41 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    totalItems: 0,
   },
   reducers: {
     addItem: (state, action) => {
-        const { name, image, cost } = action.payload;
-        const existingItem = state.items.find(item => item.name === name );
-        if(existingItem){
-            existingItem.quantity++;
-        }else{
-            state.items.push({ name, image, cost, quantity: 1});
+        const { name, cost, image } = action.payload;
+        const itemExists = state.items.find(item => item.name === name);
+        if(itemExists) {
+            itemExists.count++;
+        } else {
+            state.items.push({name, cost, image, count: 1})
         }
+        state.totalItems++;
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+        const {name} = action.payload;
+        const item = state.items.find(item => item.name === name);
+        if((state.totalItems - item.count) < 0 ) {
+            state.totalItems = 0;
+        } else {
+            state.totalItems = state.totalItems - item.count;
+        }
+        state.items = state.items.filter(item => item.name !== name);
     },
     updateQuantity: (state, action) => {
-        const { name, quantity } = action.payload;
-        const itemToUpdate = state.items.find(item => item.name === name);
-        if(itemToUpdate){
-            itemToUpdate.quantity = quantity;
+        const {name, count} = action.payload;
+        const item = state.items.find(item => item.name === name);
+        if(item) {
+            item.count = count; 
+            let totalCount = 0;
+            state.items.forEach(item => {
+                totalCount += item.count;
+            })
+            state.totalItems = totalCount;
         }
+    
     },
   },
 });
